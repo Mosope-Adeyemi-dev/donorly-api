f7481f9224eed015abae83b3b61db2c976773bd5
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2023-03-16 11:37:48 +0100

    change: include reportId to update report status

--- services/admin.service.js
+++ services/admin.service.js
@@ -5,13 +5,12 @@ const { createToken } = require("../utils/token");
 const verifyPasswordPolicy = require("../utils/passwordPolicy");
 const passwordOperations = require("../utils/password");
 const donorModel = require("../models/donor.model");
-const hospitalModel = require('../models/hospital.model');
+const hospitalModel = require("../models/hospital.model");
 const reportModel = require("../models/report.model");
 
 exports.registerAdmin = async (body) => {
   try {
-    const { name, email, password, phoneNumber } =
-      body;
+    const { name, email, password, phoneNumber } = body;
 
     if (!(await verifyPasswordPolicy(password)))
       return [false, "Password is too weak."];
@@ -63,56 +62,62 @@ exports.authenticateAdmin = async (email, password) => {
 
 exports.retrieveDonors = async () => {
   try {
-    const donors = await donorModel.find()
-    return [true, donors]
+    const donors = await donorModel.find();
+    return [true, donors];
   } catch (error) {
     console.log(error);
     return [false, translateError(error) || "Unable to retrieve donors"];
   }
-}
+};
 
 exports.retrieveHospitals = async () => {
   try {
-    const hospitals = await hospitalModel.find()
-    return [true, hospitals]
+    const hospitals = await hospitalModel.find();
+    return [true, hospitals];
   } catch (error) {
     console.log(error);
     return [false, translateError(error) || "Unable to retrieve hospitals"];
   }
-}
+};
 
 exports.retrieveReports = async () => {
   try {
-    const reports = await reportModel.aggregate([{
-      $match: {
-
-      }
-    },
+    const reports = await reportModel.aggregate([
+      {
+        $match: {
+          status: "Unresolved",
+        },
+      },
       {
         $lookup: {
           from: "hospitals",
           localField: "reportedHospital",
           foreignField: "_id",
-          as: "hospital"
-        }
-      }
-    ])
-    return [true, reports]
+          as: "hospital",
+        },
+      },
+    ]);
+    return [true, reports];
   } catch (error) {
     console.log(error);
     return [false, translateError(error) || "Unable to retrieve reports"];
   }
-}
+};
 
 exports.deactivateHospital = async (id, reportId) => {
   try {
-    const result = await hospitalModel.findByIdAndUpdate(id, { isActive: false }, {new: true})
+    const result = await hospitalModel.findByIdAndUpdate(
+      id,
+      { isActive: false },
+      { new: true }
+    );
 
-    if(reportId) await reportModel.findByIdAndUpdate(reportId, { status: "Resolved"})
-    
-    return [true, result]
+    if (reportId)
+      await reportModel.findByIdAndUpdate(reportId, { status: "Resolved" });
+
+    return [true, result];
   } catch (error) {
     console.log(error);
     return [false, translateError(error) || "Unable to retrieve reports"];
   }
-}
+};
